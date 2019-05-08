ee4a22f67d27cb4fc479da256d86860ff9da4e14
Jan Marek <jan.marek@technet.ms>
2019-05-08 10:32:46 +0200

    psd and psm fixes

--- CYB3RTools.psm1
+++ CYB3RTools.psm1
@@ -1,9 +1,23 @@
 ﻿function Set-CRConsoleTitle {
+    <#
+       .SYNOPSIS
+       Sets the console window title to the defined text.
+       
+       .DESCRIPTION
+       Sets the console window title to the defined text.
+
+       .PARAMETER TitleText
+       New title text.
+
+       .EXAMPLE
+       C:\PS> Set-CRConsoleTitle -TitleText '<<<This window is for installation>>>'
+       Configures the current console window title text to <<<This window is for installation>>>
+
+    #>
+	[cmdletbinding()]
     param(
-    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$TitleText
+    [Parameter(Mandatory=$true,HelpMessage='Enter new console title.')][ValidateNotNullOrEmpty()][string]$TitleText
     )
 
     $host.ui.RawUI.WindowTitle = $TitleText
 }
-
-Export-ModuleMember -Function Set-CRConsoleTitle
