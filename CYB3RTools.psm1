e86b135c45fd9e47d7c2ab4a5b719ea6c1f6aa7a
mcpjanmarek <jan.marek@technet.ms>
2019-10-11 16:32:29 +0200

    functions renamed and added new function to find ADS

--- CYB3RTools.psm1
+++ CYB3RTools.psm1
@@ -1,4 +1,4 @@
-﻿function Set-CRConsoleTitle {
+﻿function Set-ConsoleTitle {
     <#
        .SYNOPSIS
        Sets the console window title to the defined text.
@@ -10,7 +10,7 @@
        New title text.
 
        .EXAMPLE
-       C:\PS> Set-CRConsoleTitle -TitleText '<<<This window is for installation>>>'
+       C:\PS> Set-ConsoleTitle -TitleText '<<<This window is for installation>>>'
        Configures the current console window title text to <<<This window is for installation>>>
 
     #>
@@ -22,7 +22,7 @@
     $host.ui.RawUI.WindowTitle = $TitleText
 }
 
-function Invoke-CRScreenshot
+function Save-ScreenCapture
 {
     <#
        .SYNOPSIS
@@ -41,11 +41,11 @@ function Invoke-CRScreenshot
        Duration in milliseconds for how long the script should wait before each screenshotting cycle during the defined RepetitionDurationSeconds.
 
        .EXAMPLE
-       C:\PS> Invoke-CRScreenshot
+       C:\PS>  Save-ScreenCapture
        Initiates single screenshot for each display and save outputs to the TEMP folder.
 
        .EXAMPLE
-       C:\PS> Invoke-CRScreenshot -OutputFolder 'C:\Users\Foo\AppData\Local\SCRs' -RepetitionDurationSeconds 3600 -RepetitionWaitMilliseconds 300000
+       C:\PS>  Save-ScreenCapture -OutputFolder 'C:\Users\Foo\AppData\Local\SCRs' -RepetitionDurationSeconds 3600 -RepetitionWaitMilliseconds 300000
        Initiates screenshotting to run for 1 hour with 5 minutes waiting interval and saves output files to the folder C:\Users\Foo\AppData\Local\SCRs
     #>
     [CmdletBinding()]
@@ -71,8 +71,53 @@ function Invoke-CRScreenshot
             $GraphicsObj.Dispose()
             $BitmapObj.Save($outputpath)
         }
-        if ([int]$($stopwatch.Elapsed.TotalMilliseconds) -le $($RepetitionDurationSeconds*1000)) {Start-Sleep -Milliseconds $RepetitionWaitMilliseconds;'sleeping'}
+        if ([int]$($stopwatch.Elapsed.TotalMilliseconds) -le $($RepetitionDurationSeconds*1000)) {Start-Sleep -Milliseconds $RepetitionWaitMilliseconds}
     } until (
         [int]$($stopwatch.Elapsed.TotalMilliseconds) -gt $($RepetitionDurationSeconds*1000)
     )
+}
+
+function Find-ItemADS
+{
+    <#
+       .SYNOPSIS
+       Finds Alternate Data Streams in files in specific folder(s).
+       
+       .DESCRIPTION
+       Finds Alternate Data Streams in files in specific folder(s).
+
+       .PARAMETER Path
+       Location where to find alternate data streams.
+
+       .PARAMETER Recurse
+       Gets alternate data streams in the specified location and in all child items of the location.
+
+       .EXAMPLE
+       C:\PS>  Find-ItemADS -Path C:\Users -Recurse
+       This example finds alternate data streams in files that are located in the specified directory and its subdirectories.
+    #>
+    [CmdletBinding()]
+    Param(
+        [ValidateScript({
+            if(-Not ($_ | Test-Path) ){
+                throw "Folder does not exist" 
+            }
+            if(-Not ($_ | Test-Path -PathType Container) ){
+                throw "The Path argument must be a Folder."
+            }
+            return $true
+        })]
+        [System.IO.FileInfo]$Path,
+        [switch]$Recurse
+    )
+
+    if ($Recurse) {
+        $ObjectsToAnalyze = Get-ChildItem -Path $Path -Recurse
+    } else {
+        $ObjectsToAnalyze = Get-ChildItem -Path $Path
+    }
+
+    foreach ($ObjectToAnalyze in $ObjectsToAnalyze) {
+        $ObjectToAnalyze | Get-Item -Stream * | Select-Object -Property FileName,Stream,Length
+    }
 }
